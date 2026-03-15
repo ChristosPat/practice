3c62816e3f6e56c73832f3611e80ea8a5566c9f4
cpatelis <patelischristos@gmail.com>
2026-03-15 11:31:19 +0200

    slices

--- go2/fuctions.go
+++ go2/fuctions.go
@@ -3,14 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	numbers := [5]int{10, 20, 30, 40, 50}
-	sum := 0
+	colours := []string{"red", "blue", "green"}
 
-	fmt.Print("Στοιχεία:   ")
-	for i := range 5 {
-		fmt.Print(numbers[i], " ")
-		sum += numbers[i]
-	}
-	fmt.Println()
-	fmt.Println("Σύνολο:  ", sum)
+	colours = append(colours, "yellow", "orange")
+
+	fmt.Println(colours)
 }
