8d5f734d2c0f8394072ddc17ce3c2e575b82cd61
cpatelis <patelischristos@gmail.com>
2026-03-15 10:06:27 +0200

    if else

--- go1/hello.go
+++ go1/hello.go
@@ -5,10 +5,20 @@ import "fmt"
 func main() {
 	num1 := 15
 	num2 := 4
-
+	score := 85
 	fmt.Println(num1 + num2)
 	fmt.Println(num1 - num2)
 	fmt.Println(num1 * num2)
 	fmt.Println(num1 / num2)
 	fmt.Println(num1 % num2)
+
+	if score >= 90 {
+		fmt.Println("Excelent")
+	} else if score >= 80 {
+		fmt.Println("Very good")
+	} else if score >= 70 {
+		fmt.Println("Good")
+	} else {
+		fmt.Println("Fail")
+	}
 }
