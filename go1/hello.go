456a8d889af4ccb41883ca5e5e17a043db787f6e
cpatelis <patelischristos@gmail.com>
2026-03-15 09:57:04 +0200

    variable

---
+++ go1/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Christos"
+	var age int = 55
+	var height float64 = 1.81
+	fmt.Println("Όνομα:", name)
+	fmt.Println("Ηλικία:", age)
+	fmt.Println("Ύψος:", height)
+}
